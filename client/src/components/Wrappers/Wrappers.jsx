3599c5599f57ee04aa7d0e8cbd64621e4b355e78
rajputshashank003 <hariomshashank@gmail.com>
2025-09-12 00:41:29 +0530

    fix: create new file for wrappers

---
+++ client/src/components/Wrappers/Wrappers.jsx
@@ -0,0 +1,31 @@
+import { GoogleOAuthProvider } from '@react-oauth/google'
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from '../Hooks/useAuth'
+import CartProvider from '../Hooks/useCart'
+import App from '../../App'
+import { ToastContainer } from 'react-toastify'
+const clientId = import.meta.env.VITE_GOOGLE_SIGNIN_ID;
+
+const Wrappers = () => {
+    return (
+        <GoogleOAuthProvider clientId={clientId}>
+            <div className=' min-h-svh main_file w-screen bg-[#171717]'>
+                <BrowserRouter >
+                    <AuthProvider>
+                        <CartProvider>
+                            <App />
+                            <ToastContainer
+                                position='bottom-right' autoClose={5000} hideProgressBar={false}
+                                newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable
+                                pauseOnHover theme='light'
+                            />
+                        </CartProvider>
+                    </AuthProvider>
+                </BrowserRouter>
+            </div>
+        </GoogleOAuthProvider>
+    )
+}
+
+export default Wrappers
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,35 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import "./axiosConfig.js";
-import { AuthProvider } from './components/Hooks/useAuth.jsx';
-import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
-import { BrowserRouter } from 'react-router-dom'
-import CartProvider from './components/Hooks/useCart.jsx'
-import { LoadingProvider } from './components/Hooks/useLoading.jsx';
 import "./components/Interceptors/AuthInterceptor.jsx";
-import { GoogleOAuthProvider } from '@react-oauth/google';
-const clientId = import.meta.env.VITE_GOOGLE_SIGNIN_ID;
+import Wrappers from './components/Wrappers/Wrappers.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <>
-        <GoogleOAuthProvider clientId={clientId}>
-            <div className=' min-h-svh main_file w-screen bg-[#171717]'>
-                <BrowserRouter >
-                    <AuthProvider>
-                        <CartProvider>
-                            <App />
-                            <ToastContainer
-                                position='bottom-right' autoClose={5000} hideProgressBar={false}
-                                newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable
-                                pauseOnHover theme='light'
-                            />
-                        </CartProvider>
-                    </AuthProvider>
-                </BrowserRouter>
-            </div>
-        </GoogleOAuthProvider>
-    </>,
+    <Wrappers />,
 )
