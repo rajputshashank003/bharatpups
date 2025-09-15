d8da30e4c9371ebf6c41c4831ee3accc26a4403e
rajputshashank003 <hariomshashank@gmail.com>
2025-09-15 21:25:32 +0530

    fix: uncomment code glitch fix - 2

--- client/src/Pages/LandingPageTemp/Explore.jsx
+++ client/src/Pages/LandingPageTemp/Explore.jsx
@@ -8,7 +8,7 @@ import SignInCard from './SignInCard';
 import axios from 'axios';
 import Thumbnails_v2 from '../../components_v2/Thumbnails_v2/Thumbnails_v2';
 import { useAuth } from '../../components/Hooks/useAuth';
-// import Carousel from '../../components_v2/Carousel/Carousel';
+import Carousel from '../../components_v2/Carousel/Carousel';
 import { EXPLORE_DATA_KEY } from '../../helpers/utils';
 
 const Explore = ({ page }) => {
@@ -107,19 +107,19 @@ const Explore = ({ page }) => {
                     <div className="absolute bg-[#171717] h-[50px] w-full top-0 -translate-y-[100%] z-[9]"></div>
                     <Header_v2 />
                 </div>
-                {/* {page === 'explore' && 
+                {page === 'explore' && 
                     <div className=" mt-[24px] h-fit rounded-[12px] w-full">
                         <Carousel />
                     </div>
-                } */}
+                }
                 <div className={"flex flex-row gap-[12px] lg:gap-[16px] my-[18px] w-full overflow-scroll no-scrollbar"}>
                     {
                         loading ?
-                            [1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4].map((val) => (
-                                <div className="col-span-1 flex justify-center items-center">
+                            ([1, 2, 3, 4, 5, 6, 7, 8].map((val) => (
+                                <div key={val} className="col-span-1 flex justify-center items-center">
                                     <Chip_v2 tag={''} />
                                 </div>
-                            )) :
+                            ))) :
                             breeds?.map((val, ind) => (
                                 <div key={`key-${val?.id}-${ind}`} className="col-span-1 flex justify-center items-center">
                                     <Chip_v2 tag={val} />
