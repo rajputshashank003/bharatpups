983900354ee3ea4a7e6689d485e1bee9b1b753ec
rajputshashank003 <hariomshashank@gmail.com>
2025-09-21 19:16:15 +0530

    fix: update modal styles for better responsiveness and layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         <div className='flex flex-col  justify-center items-center'>
             {isOpen && (
                 <div className="fixed cursor-pointer inset-0 flex items-center justify-center bg-black/70 z-[9999999]">
-                    <div className="bg-white p-6 rounded-xl shadow-lg relative max-w-sm">
+                    <div className="bg-white rounded-xl p-[12px] shadow-lg relative w-[90%] max-w-[350px] h-[80%] max-h-[400px] flex items-center justify-center">
                         <button
                             className="absolute -top-[16px] -right-[12px] text-xl bg-neutral-300 p-[8px] py-[6px] hover:bg-neutral-400 duration-100 rounded-[12px] font-bold text-gray-700"
                             onClick={closeModal}
@@ -28,7 +28,7 @@ function App() {
                             onClick={handle_advertisement_click}
                             src="/AdvertisementImages/Bull15.png"
                             alt="Welcome"
-                            className="rounded-lg w-full"
+                            className="rounded-lg h-full w-full object-contain"
                         />
                     </div>
                 </div>
