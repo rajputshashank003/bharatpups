1dbc723ba3f95c0934d6e74a64b14c282df0dc34
rajputshashank003 <hariomshashank@gmail.com>
2025-09-23 21:36:44 +0530

    fix: add animation in skeleton of cards

--- client/src/components/Loader_Skeletons/FoodPageSkeleton.jsx
+++ client/src/components/Loader_Skeletons/FoodPageSkeleton.jsx
@@ -8,7 +8,7 @@ const FoodPageSkeleton = () => {
             <div className={" flex w-full flex-col justify-start gap-[22px]"}>
                 <div className={" h-[400px] rounded-[12px] m-0 max-md:w-full overflow-hidden relative  "}>
                     <motion.div
-                        className="relative h-full w-full inline-block bg-gradient-to-r overflow-hidden from-zinc-900 via-gray-700 to-neutral-950 text-white px-3 py-1 rounded-[12px] text-xs font-semibold"
+                        className="relative h-full w-full inline-block bg-gradient-to-r overflow-hidden from-zinc-950 via-neutral-700 to-neutral-950 text-white px-3 py-1 rounded-[12px] text-xs font-semibold"
                         style={{
                             backgroundSize: '300% 300%',
                         }}
@@ -25,13 +25,13 @@ const FoodPageSkeleton = () => {
                         <motion.div
                             className="absolute inset-0 w-full h-full"
                             style={{
-                                background: 'linear-gradient(90deg, transparent 30%, rgba(255,255,255,0.3) 50%, transparent 70%)',
+                                background: 'linear-gradient(90deg, transparent 30%, rgba(255,255,255,0.2) 50%, transparent 70%)',
                                 backgroundSize: '200% 180%',
                                 rotate: '-35deg',
-                                width: '240%',
-                                height: '250%',
-                                left: '0%',
-                                top: '-60%',
+                                width: '320%',
+                                height: '350%',
+                                left: '-40%',
+                                top: '-150%',
                             }}
                             animate={{
                                 backgroundPosition: ["-100% 0%", "100% 0%"]
@@ -45,23 +45,23 @@ const FoodPageSkeleton = () => {
                     </motion.div>
                 </div>
                 <div className={" flex flex-col gap-[12px] max-md:mt-[18px] "}>
-                    <h1 className={" flex w-[50%] bg-neutral-800 rounded-[12px] h-[34px] justify-between gap-[12px] items-center"}>
+                    <h1 className={" flex w-[50%] animate-pulse duration-75 bg-neutral-800 rounded-[12px] h-[34px] justify-between gap-[12px] items-center"}>
                        
                     </h1>
-                    <div className='bg-neutral-800 h-[32px] w-[50px] px-[12px] rounded-[6px] py-[8px]'>
+                    <div className='bg-neutral-800 animate-pulse duration-75 h-[32px] w-[50px] px-[12px] rounded-[6px] py-[8px]'>
                     </div>
                     <div className='flex flex-wrap gap-[16px]'>
-                        <div className='bg-neutral-800 h-[32px] w-[50px] px-[12px] rounded-[6px] py-[8px]'>
+                        <div className='bg-neutral-800 animate-pulse duration-75 h-[32px] w-[50px] px-[12px] rounded-[6px] py-[8px]'>
                         </div>
-                        <div className='bg-neutral-800 h-[32px] w-[50px] px-[12px] rounded-[6px] py-[8px]'>
+                        <div className='bg-neutral-800 animate-pulse duration-75 h-[32px] w-[50px] px-[12px] rounded-[6px] py-[8px]'>
                         </div>
-                        <div className='bg-neutral-800 h-[32px] w-[50px] px-[12px] rounded-[6px] py-[8px]'>
+                        <div className='bg-neutral-800 animate-pulse duration-75 h-[32px] w-[50px] px-[12px] rounded-[6px] py-[8px]'>
                         </div>
                     </div>
                 </div>
-                <div className="text-white w-full h-[100px] bg-neutral-800 rounded-[12px] ">
+                <div className="text-white animate-pulse duration-75 w-full h-[100px] bg-neutral-800 rounded-[12px] ">
                 </div>
-                <div className='bg-neutral-800 h-[34px] w-[60px] px-[12px] rounded-[6px] py-[8px]'>
+                <div className='bg-neutral-800 animate-pulse duration-75 h-[40px] w-[100px] px-[12px] rounded-[6px] py-[8px]'>
                 </div>
             </div>
         </div>
