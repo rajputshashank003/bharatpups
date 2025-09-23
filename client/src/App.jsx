17ed0ff31413bb3ba070ffb37cfe120124036954
rajputshashank003 <hariomshashank@gmail.com>
2025-09-23 21:59:28 +0530

    feat: add vercel analytics

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import AppRoutes from './AppRoutes.jsx';
 import Loading from './components/Loading/Loading.jsx';
 import BottomBar from './components_v3/BottomBar.jsx';
 import AdvertisementBanner from './components/AdvertisementBanner/AdvertisementBanner.jsx';
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
 
     return (
         <div className='flex flex-col  justify-center items-center'>
             <div className='lg:max-w-[1200px] w-screen mx-[40px]'>
+                <Analytics />
                 <AdvertisementBanner />
                 <Loading />
                 <div className=''>
